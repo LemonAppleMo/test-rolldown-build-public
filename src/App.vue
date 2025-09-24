1e7d21f054fd2be9a6676ec497396b47fbf65962
He Xinpeng <hexinpeng1130@gmail.com>
2025-09-24 14:41:14 +0800

    feat: simplify the main code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import { defineAsyncComponent, ref } from 'vue'
-
-const i = ref(0)
-const components = [
-  () => import('./components/Comp1.vue'),
-  () => import('./components/Comp2.vue'),
-  () => import('./components/Comp3.vue')
-].map(defineAsyncComponent)
+import('./components/Comp1.vue')
+import('./components/Comp2.vue')
+import('./components/Comp3.vue')
 </script>
 
 <template>
-  <div>
-    <button v-for="item in components.length" @click="i = item - 1">
-      {{ item }}
-    </button>
-  </div>
-  <component :is="components[i]" />
+  <div></div>
 </template>
